8992b61d24e2c35fa13b238329837d9e6b3be5b7
ming <850209126@qq.com>
2022-04-09 09:23:00 +0800

    更新版本，增加大量css3，减少了js操作

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,36 +1,20 @@
-const indexImg = document.getElementsByClassName('indexImg')[0]
-const showMe = document.getElementsByClassName('showMe')[0]
-let count = 0;
-let countEvent = function (e) {
-    if (count === 0) {
-        this.children[1].classList.add('maskShow')
-        setTimeout(() => {
-            count++;
-        }, 500);
+const [swithDivContain] = document.getElementsByClassName('swithDivContain')
+const [indexImgDiv] = document.getElementsByClassName('indexImg')
+const [showMeDiv] = document.getElementsByClassName('showMe')
 
-    } else {
-        this.style.opacity = '0'
-        showMe.classList.add('showMeUp')
+const swithDiv = (e) => {
+    console.log(e.wheelDelta);
+    if (e.wheelDelta < 0) {
+        indexImgDiv.classList.add('indexImgMoveup')
+        showMeDiv.classList.add('showMeMoveup')
+    } else if (e.wheelDelta > 0) {
+        indexImgDiv.classList.remove('indexImgMoveup')
+        showMeDiv.classList.remove('showMeMoveup')
     }
 }
-indexImg.addEventListener('mousewheel', countEvent)
-indexImg.addEventListener('touchmove', countEvent)
 
-const headImg = document.getElementsByClassName('headImg')[0]
-const jokeItem = document.getElementsByClassName('jokeItem')[0]
-const jokeList = document.getElementsByClassName('jokeList')[0]
-function getRandom(min, max) { return parseInt(Math.random() * (max - min + 1)) + min; }
-headImg.addEventListener('click', function (e) {
-    jokeList.classList.add('jokeShow')
-    var xhr = new XMLHttpRequest()
-    xhr.open('get', 'https://api.gugudata.com/news/joke/demo')
-    xhr.send(null)
-    xhr.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            console.log(JSON.parse(this.responseText))
-            let jokeData = JSON.parse(this.responseText).Data
-            let jokeRand = getRandom(0, jokeData.length)
-            jokeItem.innerHTML = jokeData[jokeRand].Content
-        }
-    }
-})
+setTimeout(() => {
+    swithDivContain.addEventListener('mousewheel', swithDiv)
+    swithDivContain.addEventListener('touchend', swithDiv)
+}, 2000);
+
